da1a35f5a4bd285cf032b20e93043b55359f25b2
Anthony Kimani <kimaniantoni@gmail.com>
2022-11-24 07:37:03 +0300

    learned about numbers, strings and there methods

--- 03_numbers.php
+++ 03_numbers.php
@@ -16,20 +16,45 @@ echo $a % $b. ' <br> ';
 
 // Assignment with math operators
 
-$a += $b. ' <br> ';
-$a -= $b. ' <br> ';
-$a *= $b. ' <br> ';
+// $a += $b. ' <br> ';
+// $a -= $b. ' <br> ';
+// $a *= $b. ' <br> ';
 
 // Increment operator
 
+echo $a++. ' <br> ';
+
 // Decrement operator
 
+echo $b--. ' <br> ';
 // Number checking functions
+
+is_float(1.25); //true
+is_double(2.34); //true
+is_int(4); //true
+is_numeric("3.45"); //true
 
 // Conversion
 
+$strNumber = "12.405";
+$number = (float)$strNumber;
+//$number = intval($strNumber);
+var_dump($number);
+
 // Number functions
 
+echo "abs(-15)". abs(-15). ' <br> '; // prints absolute value;
+echo "pow(2,3)". pow(2,3). ' <br> '; // raises the value of 2 to the power of 3
+echo "sqrt(16)". sqrt(16). ' <br> '; // finds the sqrt of a number 16 to be 4
+echo "max(2, 9, 3)". max(2, 9, 3). ' <br> '; // finds the maximum value of a number to be 9
+echo "min(2, 3)". min(2, 3). ' <br> '; // finds the minimum value of a number to be 2
+echo "round(2.4)". round(2.4). ' <br> '; // rounds the number up to the nearest whole number which is 2
+echo "round(2.6)". round(2.6). ' <br> '; // rounds the number to the nearest value 3
+echo "floor(2.6)". floor(2.6). ' <br> '; // rounds a value down to the nearest whole number
+echo "ceil(2.4)". ceil(2.4). ' <br> '; // rounds a value up to the nearest whole number
+
 // Formatting numbers
+$number = 23452345652.3456523345; 
+echo number_format($number, 2, ",", '');
 
 // https://www.php.net/manual/en/ref.math.php
--- 04_strings.php
+++ 04_strings.php
@@ -2,11 +2,55 @@
 
 // Create simple string
 
+$string = "Hello Kimani";
+$string2 = "Hello Liz";
+
+$concat = "Hello I am".$string.", I am 22 years old";
+
+$interpolation = "Hello I am $string, a 22 year old man ";
+$single = 'Hello I am $string , a 22 year old man';
+
+echo $concat. ' <br> ';
+echo $interpolation. ' <br> ';
+echo $single. ' <br> ';
+
 // String concatenation
+
+echo "Hello"."World"."and PHP";
 
 // String functions
 
+$hello = "      Hello World     ";
+
+echo "1- ". strlen($hello). ' <br> '; //prints length of the string
+echo "2- ". trim($hello). ' <br> '; // removes white spaces from the string
+echo "3- ". ltrim($hello). ' <br> '; // removes white space from the left side of the string
+echo "4- ". rtrim($hello). ' <br> '; // removes white space from the right side of the string
+echo "5- ". str_word_count($hello). ' <br> '; // counts the number of words in a string
+echo "6- ". strrev($hello). ' <br> '; // reverses the word
+echo "7- ". strtoupper($hello). ' <br> '; // prints an uppercased string
+echo "8- ". strtolower($hello). ' <br> '; // prints a lowercased string
+echo "9- ". ucfirst('hello'). ' <br> '; //makes a strings first letter uppercase
+echo "10- ". lcfirst('HELLO'). ' <br> '; // makes a string's first letter lowercased
+echo "11- ". ucwords("hello world and php"). ' <br> '; // uppercases the first letter in any word on the string
+echo "12- ". strpos($string, "kimani"). ' <br> '; // finds a match of the word in the first argument
+echo "13- ". stripos($hello, "world"). ' <br> '; // finds a match of the word and prints out its string , even if it lowercased
+echo "14- ". substr($hello, 8, 3). ' <br> '; // prints the string from line 8 and 3 letters
+echo "15- ". str_replace("World", "PHP", $hello). ' <br> '; // searches and replaces the word World with PHP , in the string $hello
+echo "16- ". str_ireplace("World", "php", $hello). ' <br> '; // searches , replaces words in string ignoring case sensitivity
+
+
+
 // Multiline text and line breaks
+
+$long_text = "
+Hello, my name is Kimani
+I am 22, 
+I love writing eloquent php
+";
+
+echo $long_text. ' <br> ';
+echo nl2br($long_text). ' <br> ';
 
 // Multiline text and reserve html tags
 
