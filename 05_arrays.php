32d9a1f25cfc9b69e65988edb01c19df9c2b782d
Anthony Kimani <kimaniantoni@gmail.com>
2022-11-25 05:17:55 +0300

    learned how to make arrays in php and there respective array methods

--- 05_arrays.php
+++ 05_arrays.php
@@ -2,37 +2,130 @@
 
 // Create array
 
+$fruits = ["Banana", "Apple", "Orange"];
+
 // Print the whole array
+
+echo '<pre>';
+var_dump($fruits);
+echo '<pre>';
 
 // Get element by index
 
+echo $fruits[2].'<br>';
+
 // Set element by index
+
+$fruits[0] = "Peach";
+echo '<pre>';
+var_dump($fruits);
+echo '<pre>';
 
 // Check if array has element at index 2
 
+isset($fruits[3]); //false
+
 // Append element
+$fruits[] = "Banana";
+
+echo '<pre>';
+var_dump($fruits);
+echo '<pre>';
 
 // Print the length of the array
 
+echo count($fruits).'<br>';
+
 // Add element at the end of the array
+
+array_push($fruits, "Strawberry");
+
+echo '<pre>';
+var_dump($fruits);
+echo '<pre>';
 
 // Remove element from the end of the array
 
+echo array_pop($fruits);
+
+echo '<pre>';
+var_dump($fruits);
+echo '<pre>';
+
 // Add element at the beginning of the array
+
+echo array_unshift($fruits, "Mango");
+
+echo '<pre>';
+var_dump($fruits);
+echo '<pre>';
+
 
 // Remove element from the beginning of the array
 
+echo array_shift($fruits);
+
+echo '<pre>';
+var_dump($fruits);
+echo '<pre>';
+
+
 // Split the string into an array
+
+echo $string = "Avocado, Mango, Passion";
+
+echo '<pre>';
+var_dump(explode(",",$string));
+echo '<pre>';
+
 
 // Combine array elements into string
 
+echo implode(",", $fruits);
+
 // Check if element exist in the array
+
+echo '<pre>';
+var_dump(in_array("Mango", $fruits));
+echo '<pre>';
+
 
 // Search element index in the array
 
+echo '<pre>';
+var_dump(array_search("Apple", $fruits));
+echo "</pre>";
+
 // Merge two arrays
 
+$vegetables = ["potato", "cucumber"];
+
+$legumes = ["beans", "peas"];
+
+echo "<pre>";
+var_dump(array_merge($vegetables, $legumes));
+var_dump([...$vegetables, ...$legumes]); // spreads the array
+echo "</pre>";
+
 // Sorting of array (Reverse order also)
+
+echo "</pre>";
+var_dump($fruits);
+echo "</pre>";
+sort($fruits); // sorts aphlabetically acsending
+echo "</pre>";
+rsort($fruits); // sort alphabetically descending
+echo "</pre>";
+var_dump($fruits);
+
+$numbers = [3,4,5,2,5,2,4];
+echo "</pre>";
+var_dump($numbers);
+echo "</pre>";
+sort($numbers);
+echo "</pre>";
+var_dump($numbers);
+echo "</pre>";
 
 
 // https://www.php.net/manual/en/ref.array.php
