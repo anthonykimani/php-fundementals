9c91561fabf6c349457f5bba4bc5a07213694631
Anthony Kimani <kimaniantoni@gmail.com>
2022-11-25 06:10:36 +0300

    practice use of conditional statements such as if else , switch statements

--- 06_conditionals.php
+++ 06_conditionals.php
@@ -4,21 +4,71 @@ $age = 20;
 $salary = 300000;
 
 // Sample if
+if ($age === 20){
+    echo "1"."<br>";
+}
 
 // Without circle braces
+if ($age === 20) echo "3"."<br>";
+
 
 // Sample if-else
-
+if($age > 20){
+    echo "greater than 20"."<br>";
+}else{
+    echo "less than 20"."<br>";
+}
 // Difference between == and ===
+
+if($age == "20"){
+    echo "loosely equals 20"."<br>";
+}
+
+if($age === 20){
+    echo "strictly equals 20"."<br>";
+}else{
+    echo "loosely equals 20"."<br>";
+}
 
 // if AND
 
+if($age > 20 && $salary === 300000){
+    echo "Do Something"."<br>";
+}else{
+    echo "Cant Do anything"."<br>";
+}
+
 // if OR
+
+if($age > 20 || $salary === 300000){
+    echo "Do Something"."<br>";
+}else{
+    echo "Cant Do anything"."<br>";
+}
 
 // Ternary if
 
+echo $age < 22 ? "Antony the Younger" : "Antony the Elder"."<br>";
+
 // Short ternary
+
+$age?: "18";
 
 // Null coalescing operator
 
 // switch
+
+$userRole = "admin";
+switch($userRole){
+    case "admin":
+        echo "admin"."<br>";
+        break;
+    case "editor":
+        echo "editor"."<br>";
+        break;
+    case "user":
+        echo "user"."<br>";
+        break;
+    default:
+        echo "Invalid role"."<br>";
+}
