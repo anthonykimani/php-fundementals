eabbe4c5a46c12bc91f024e8e13df55204b4dbd6
Anthony Kimani <kimaniantoni@gmail.com>
2022-11-25 05:53:14 +0300

    practised associative arrays in php and their methods

--- 05_arrays.php
+++ 05_arrays.php
@@ -2,130 +2,130 @@
 
 // Create array
 
-$fruits = ["Banana", "Apple", "Orange"];
+// $fruits = ["Banana", "Apple", "Orange"];
 
-// Print the whole array
+// // Print the whole array
 
-echo '<pre>';
-var_dump($fruits);
-echo '<pre>';
+// echo '<pre>';
+// var_dump($fruits);
+// echo '<pre>';
 
-// Get element by index
+// // Get element by index
 
-echo $fruits[2].'<br>';
+// echo $fruits[2].'<br>';
 
-// Set element by index
+// // Set element by index
 
-$fruits[0] = "Peach";
-echo '<pre>';
-var_dump($fruits);
-echo '<pre>';
+// $fruits[0] = "Peach";
+// echo '<pre>';
+// var_dump($fruits);
+// echo '<pre>';
 
-// Check if array has element at index 2
+// // Check if array has element at index 2
 
-isset($fruits[3]); //false
+// isset($fruits[3]); //false
 
-// Append element
-$fruits[] = "Banana";
+// // Append element
+// $fruits[] = "Banana";
 
-echo '<pre>';
-var_dump($fruits);
-echo '<pre>';
+// echo '<pre>';
+// var_dump($fruits);
+// echo '<pre>';
 
-// Print the length of the array
+// // Print the length of the array
 
-echo count($fruits).'<br>';
+// echo count($fruits).'<br>';
 
-// Add element at the end of the array
+// // Add element at the end of the array
 
-array_push($fruits, "Strawberry");
+// array_push($fruits, "Strawberry");
 
-echo '<pre>';
-var_dump($fruits);
-echo '<pre>';
+// echo '<pre>';
+// var_dump($fruits);
+// echo '<pre>';
 
-// Remove element from the end of the array
+// // Remove element from the end of the array
 
-echo array_pop($fruits);
+// echo array_pop($fruits);
 
-echo '<pre>';
-var_dump($fruits);
-echo '<pre>';
+// echo '<pre>';
+// var_dump($fruits);
+// echo '<pre>';
 
-// Add element at the beginning of the array
+// // Add element at the beginning of the array
 
-echo array_unshift($fruits, "Mango");
+// echo array_unshift($fruits, "Mango");
 
-echo '<pre>';
-var_dump($fruits);
-echo '<pre>';
-
-
-// Remove element from the beginning of the array
-
-echo array_shift($fruits);
-
-echo '<pre>';
-var_dump($fruits);
-echo '<pre>';
+// echo '<pre>';
+// var_dump($fruits);
+// echo '<pre>';
 
 
-// Split the string into an array
+// // Remove element from the beginning of the array
 
-echo $string = "Avocado, Mango, Passion";
+// echo array_shift($fruits);
 
-echo '<pre>';
-var_dump(explode(",",$string));
-echo '<pre>';
-
-
-// Combine array elements into string
-
-echo implode(",", $fruits);
-
-// Check if element exist in the array
-
-echo '<pre>';
-var_dump(in_array("Mango", $fruits));
-echo '<pre>';
+// echo '<pre>';
+// var_dump($fruits);
+// echo '<pre>';
 
 
-// Search element index in the array
+// // Split the string into an array
 
-echo '<pre>';
-var_dump(array_search("Apple", $fruits));
-echo "</pre>";
+// echo $string = "Avocado, Mango, Passion";
 
-// Merge two arrays
+// echo '<pre>';
+// var_dump(explode(",",$string));
+// echo '<pre>';
 
-$vegetables = ["potato", "cucumber"];
 
-$legumes = ["beans", "peas"];
+// // Combine array elements into string
 
-echo "<pre>";
-var_dump(array_merge($vegetables, $legumes));
-var_dump([...$vegetables, ...$legumes]); // spreads the array
-echo "</pre>";
+// echo implode(",", $fruits);
 
-// Sorting of array (Reverse order also)
+// // Check if element exist in the array
 
-echo "</pre>";
-var_dump($fruits);
-echo "</pre>";
-sort($fruits); // sorts aphlabetically acsending
-echo "</pre>";
-rsort($fruits); // sort alphabetically descending
-echo "</pre>";
-var_dump($fruits);
+// echo '<pre>';
+// var_dump(in_array("Mango", $fruits));
+// echo '<pre>';
 
-$numbers = [3,4,5,2,5,2,4];
-echo "</pre>";
-var_dump($numbers);
-echo "</pre>";
-sort($numbers);
-echo "</pre>";
-var_dump($numbers);
-echo "</pre>";
+
+// // Search element index in the array
+
+// echo '<pre>';
+// var_dump(array_search("Apple", $fruits));
+// echo "</pre>";
+
+// // Merge two arrays
+
+// $vegetables = ["potato", "cucumber"];
+
+// $legumes = ["beans", "peas"];
+
+// echo "<pre>";
+// var_dump(array_merge($vegetables, $legumes));
+// var_dump([...$vegetables, ...$legumes]); // spreads the array
+// echo "</pre>";
+
+// // Sorting of array (Reverse order also)
+
+// echo "</pre>";
+// var_dump($fruits);
+// echo "</pre>";
+// sort($fruits); // sorts aphlabetically acsending
+// echo "</pre>";
+// rsort($fruits); // sort alphabetically descending
+// echo "</pre>";
+// var_dump($fruits);
+
+// $numbers = [3,4,5,2,5,2,4];
+// echo "</pre>";
+// var_dump($numbers);
+// echo "</pre>";
+// sort($numbers);
+// echo "</pre>";
+// var_dump($numbers);
+// echo "</pre>";
 
 
 // https://www.php.net/manual/en/ref.array.php
@@ -136,19 +136,55 @@ echo "</pre>";
 
 // Create an associative array
 
+$person = [
+    "name" => "Antony",
+    "surname" => "Kimani",
+    "age" => 29,
+    "hobbies" => ["Racing", "Bantering", "Swimming"]
+];
+
+echo '<pre>';
+var_dump($person);
+print_r($person);
+echo '</pre>';
+
 // Get element by key
+echo $person['name'].'<br>';
 
 // Set element by key
+echo $person['name'] = "Priest".'<br>';
+echo $person['profession'] = "Software Engineer"."<br>";
 
 // Null coalescing assignment operator. Since PHP 7.4
+if (!isset($person['address'])) {
+    $person['address'] = 'unknown';
+}
+
+$person['address'] ??= 'unknown';
 
 // Check if array has specific key
 
 // Print the keys of the array
+echo "</pre>";
+var_dump(array_keys($person)).'<br>';
+echo "</pre>";
 
 // Print the values of the array
 
+echo "</pre>";
+var_dump(array_values($person)).'<br>';
+echo "</pre>";
+
+
 // Sorting associative arrays by values, by keys
+
+ksort($person); // sorts the associative array by keys
+asort($person); // sorts by values
 
 
 // Two dimensional arrays
+
+$todos = [
+    ["title" => "todo title 1", "completed"],
+
+];
